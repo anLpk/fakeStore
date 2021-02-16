f11c1a841d023624352ed11d5cc868b9d35b8dfd
anil pak <anilpak35@gmail.com>
2021-02-16 12:06:36 +1100

    select2 installed and all setup completed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,7 @@
 import { initFlatpickr } from "./plugins/flatpickr";
 import { flexPanel } from "./plugins/panel";
 import { playMusic } from "./plugins/music";
+import { multipleSelect } from "./plugins/init_select2";
 
 require("@rails/ujs").start();
 require("turbolinks").start();
@@ -9,9 +10,11 @@ require("channels");
 
 import "controllers";
 import "bootstrap";
+import "select2/dist/css/select2.css";
 
 document.addEventListener("turbolinks:load", () => {
   initFlatpickr();
   flexPanel();
   playMusic();
+  multipleSelect();
 });
